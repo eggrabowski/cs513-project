e5497cb37cbb56367d51522b7e1ef9aa89f4491e
Bo Li <bli54@hawk.iit.edu>
2016-05-01 21:20:26 -0500

    new final
    new txt

--- result1/main.m
+++ result1/main.m
@@ -1,7 +1,7 @@
 mydir = 'D:\cs513-project\result1';
 cd(mydir);
 
-filename = '190.txt';
+filename = 'final.txt';
 
 display('TODO');
 
@@ -10,5 +10,6 @@ data = importdata(filename,',');
 x = data(:,1);
 y = data(:,2);
 
-a = 5;
-scatter(x,y,a,'filled')
+a = 1;
+
+scatter(x,y,a,'filled');
