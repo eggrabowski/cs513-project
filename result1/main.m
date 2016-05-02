bd48116fa6346772b149938cfe854c4a9256185b
sergiogilupm <sergio.gil.luque@gmail.com>
2016-05-02 13:57:16 -0500

    Updating code to draw 3d plotting

--- result1/main.m
+++ result1/main.m
@@ -1,14 +1,19 @@
-mydir = 'D:\cs513-project\result1';
+mydir = '/Users/guil/repo/cs513-project/result1';
 cd(mydir);
 
 filename = '190.txt';
 
-display('TODO');
+display ('TODO');
 
 data = importdata(filename,',');
 
-x = data(:,1);
-y = data(:,2);
+x = data(1:100000,1);
+y = data(1:100000,2);
+z = data(1:100000,3);
 
 a = 5;
-scatter(x,y,a,'filled')
+
+% 3d scattering. Right now it only takes a subset of the first 100000 cloud
+% points and fills them
+
+scatter3(x,y,z, 'filled');   
